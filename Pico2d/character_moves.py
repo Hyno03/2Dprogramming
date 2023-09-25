adad5c30f1a921d7969775e2fb44554cb6d738c7
Hyno03 <jenny2117@naver.com>
2023-09-25 11:22:18 +0900

    Update character_moves.py
    애니메이션을 사용하며 캐릭터가 이동하는 코드 작성

--- Pico2d/character_moves.py
+++ Pico2d/character_moves.py
@@ -3,14 +3,16 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('run_animation.png')
 
-for x in range(0, 800, 5):
+frame = 0
+for x in range(0, 800, 10):
     clear_canvas()
     grass.draw(400,30)
-    character.draw(x,90)
+    character.clip_draw(frame*100, 0 , 100, 100, x , 90)
     update_canvas()
-    delay(0.01)
+    frame = (frame + 1) % 8
+    delay(0.05)
 
 close_canvas()
 
