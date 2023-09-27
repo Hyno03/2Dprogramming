ff93d4eb903f24ed64bccc8aefa678d39b845ffc
Hyno03 <jenny2117@naver.com>
2023-09-27 17:40:55 +0900

    Add resource
    수업 자료 추가

--- Pico2d/move_character_with_mouse.py
+++ Pico2d/move_character_with_mouse.py
@@ -1,43 +1,40 @@
 from pico2d import *
 
-
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
-open_canvas(TUK_WIDTH,TUK_HEIGHT)
-tuk_ground = load_image('TUK_GROUND.png')
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
-
+TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
 
 def handle_events():
     global running
-    global x,y
+    global x, y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x,y = event.x, TUK_HEIGHT - 1 - event.y
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+    pass
+
 
 
 running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
-x, y = TUK_WIDTH//2, TUK_HEIGHT//2
 hide_cursor()
-
 
 while running:
     clear_canvas()
-
-    tuk_ground.draw(TUK_WIDTH//2, TUK_HEIGHT//2)
-    character.clip_draw(frame * 100, 100, 100, 100, x, y)
-
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
-    handle_events()
     frame = (frame + 1) % 8
-    delay(0.05)
+
+    handle_events()
 
 close_canvas()
 
