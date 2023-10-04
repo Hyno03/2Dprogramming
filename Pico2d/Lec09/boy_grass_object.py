4a1f7776c71c34851ba27e364e768f5d0973a8d9
Hyno03 <jenny2117@naver.com>
2023-10-04 18:34:28 +0900

    Made Grass class

--- Pico2d/Lec09/boy_grass_object.py
+++ Pico2d/Lec09/boy_grass_object.py
@@ -2,7 +2,15 @@ from pico2d import *
 
 
 # Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
 
+    def draw(self):
+        self.image.draw(400,30)
+
+    def update(self):
+        pass
 def handle_events():
     global running
     events = get_events()
