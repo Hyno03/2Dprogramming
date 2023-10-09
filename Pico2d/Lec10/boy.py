7cc5b1cfc582a9ac98472d9e97b2ece1f9227fb1
Hyno03 <jenny2117@naver.com>
2023-10-09 12:46:56 +0900

    Code StateMachine and Idle

--- Pico2d/Lec10/boy.py
+++ Pico2d/Lec10/boy.py
@@ -13,30 +13,45 @@ class Boy:
 
     def update(self):
         self.frame = (self.frame + 1) % 8
+        self.state_machine.update()
 
     def handle_event(self, event):
         pass
 
     def draw(self):
         self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
+        self.state_machine.draw()
 
 
 class Idle:
     @staticmethod
     def enter():
+        print('Idle Entered')
         pass
 
     @staticmethod
     def exit():
+        print('Idle Exit')
         pass
 
     @staticmethod
     def do():
+        print('Idle Do')
         pass
 
 
 class StateMachine:
     def __init__(self):
+        self.cur_state = Idle
         pass
+
     def start(self):
+        self.cur_state.enter()
+        pass
+
+    def update(self):
+        self.cur_state.do()
+        pass
+
+    def draw(self):
         pass
