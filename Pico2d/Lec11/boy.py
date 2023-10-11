5ba7ac3fdb7851050204e667ff07934b75646cf0
Hyno03 <jenny2117@naver.com>
2023-10-11 19:09:55 +0900

    copy code

--- Pico2d/Lec11/boy.py
+++ Pico2d/Lec11/boy.py
@@ -1,11 +1,27 @@
 # 이것은 각 상태들을 객체로 구현한 것임.
 
-from pico2d import load_image, SDL_KEYDOWN, SDLK_SPACE, get_time
+from pico2d import load_image, SDL_KEYDOWN, SDL_KEYUP, SDL_KEYDOWN, SDLK_SPACE, SDLK_LEFT, SDLK_RIGHT, get_time
 import math
 
 
 def space_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
 
 def time_out(e):
@@ -15,11 +31,15 @@ def time_out(e):
 class Idle:
 
     @staticmethod
-    def enter(boy):
+    def enter(boy, e):
+        if boy.action == 0:
+            boy.action = 2
+        elif boy.action == 1:
+            boy.action = 3
         boy.idle_start_time = get_time()
 
     @staticmethod
-    def exit(boy):
+    def exit(boy, e):
         pass
 
     @staticmethod
@@ -34,14 +54,39 @@ class Idle:
                             boy.x, boy.y)
 
 
-class Sleep:
+class Run:
 
     @staticmethod
-    def enter(boy):
+    def enter(boy, e):
+        if right_down(e) or left_up(e):
+            boy.dir, boy.action = 1, 1
+        elif left_down(e) or right_up(e):
+            boy.dir, boy.action = -1, 0
+
+    @staticmethod
+    def exit(boy, e):
         pass
 
     @staticmethod
-    def exit(boy):
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.x += boy.dir * 5
+        pass
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100,
+                            boy.x, boy.y)
+
+
+class Sleep:
+
+    @staticmethod
+    def enter(boy, e):
+        pass
+
+    @staticmethod
+    def exit(boy, e):
         pass
 
     @staticmethod
@@ -56,15 +101,16 @@ class Sleep:
 
 class StateMachine:
     def __init__(self, boy):
-        self.cur_state = Sleep
+        self.cur_state = Idle
         self.boy = boy
         self.transitions = {
-            Sleep: {space_down: Idle},
-            Idle: {time_out: Sleep}
+            Idle: {right_down: Run, right_up: Run, left_down: Run, left_up: Run, time_out: Sleep},
+            Run: {right_down: Idle, right_up: Idle, left_up: Idle, left_down: Idle},
+            Sleep: {right_down: Run, right_up: Run, left_down: Run, left_up: Run, space_down: Idle}
         }
 
     def start(self):
-        self.cur_state.enter(self.boy)
+        self.cur_state.enter(self.boy, ('NONE', 0))
 
     def update(self):
         self.cur_state.do(self.boy)
@@ -72,9 +118,9 @@ class StateMachine:
     def handle_event(self, e):
         for check_event, next_state in self.transitions[self.cur_state].items():
             if check_event(e):
-                self.cur_state.exit(self.boy)
+                self.cur_state.exit(self.boy, e)
                 self.cur_state = next_state
-                self.cur_state.enter(self.boy)
+                self.cur_state.enter(self.boy, e)
                 return True
             return False
 
